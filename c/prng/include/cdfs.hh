b7f455c78ac13e030455e8affe0fc157bb36648c
Kristian Hahn <khahn@cern.ch>
2023-04-12 10:07:36 -0500

    move c/prng to cpp/probability

--- c/prng/include/cdfs.hh
+++
@@ -1,9 +0,0 @@
-#ifndef PRNG_CDFS
-#define PRNG_CDFS
-
-double cdf_exponential( double x, double* params );
-
-double cdf_gaussian( double x, double* params );
-
-
-#endif
---
+++ cpp/probability/src/cdfs.cc
@@ -0,0 +1,47 @@
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <boost/math/special_functions/erf.hpp>
+#include "cdfs.hh"
+
+
+// -----------------------------------------------------------------------------
+// exponential CDF
+// -----------------------------------------------------------------------------
+double 
+cdf_exponential( double x, double* params ) 
+// -----------------------------------------------------------------------------
+{ 
+  double lambda = params[0];
+  return 1. - exp(-lambda*x);
+}
+
+
+// -----------------------------------------------------------------------------
+// gaussian CDF
+// -----------------------------------------------------------------------------
+double 
+cdf_gaussian( double x, double* params ) 
+// -----------------------------------------------------------------------------
+{ 
+  double mu = params[0];
+  double sigma = params[1];
+  double arg_erf = (x-mu)/(sqrt(2)*sigma);
+  return 0.5*( 1. + erf(arg_erf));
+}
+
+// -----------------------------------------------------------------------------
+// gaussian CDF
+// -----------------------------------------------------------------------------
+double 
+inv_cdf_gaussian( double y, double* params ) 
+// -----------------------------------------------------------------------------
+{ 
+  double mu = params[0];
+  double sigma = params[1];
+  double arg_erf_inv = 2*y-1.;
+  return sigma*sqrt(2)*boost::math::erf_inv(arg_erf_inv) + mu;
+}
+
+
+
