3143512c1bf10943e5d46e8efda48afe68f706c4
Marco Wang <marcowang01@gmail.com>
2023-04-18 16:38:27 -0500

    add instructions

--- cpp/probability/test/test_maxwell_boltzmann_metropolis.cc
+++ cpp/probability/test/test_maxwell_boltzmann_metropolis.cc
@@ -4,6 +4,12 @@
 
 unsigned count_reject=0, count_metro=0;
 
+
+//----------------------------------------------
+// usage: ./test_maxwell_boltzmann_metropolis [random seed] [a] [test_cdf]
+// example: ./test_maxwell_boltzmann_metropolis $RANDOM 1.0 1 > maxwell_boltzmann.dat
+// ---------------------------------------------
+
 int main(int argc, char** argv) {
 
   if( argc != 2 ) {
@@ -12,17 +18,29 @@ int main(int argc, char** argv) {
   }
 
   srand(strtoul(argv[1],NULL,10));
+
+  double a = 1;
+  if ( argc > 2 ) {
+    a = strtod(argv[2],NULL);
+  }
+
+  bool test_cdf = false;
+  if ( argc > 3 ) {
+    test_cdf = true;
+  }
   
   double range_i = 0.;
   double range_f =  20.;
-  double a      = 1;
   double params[1] = {a};
 
   double x_metro = range_i + rand()*((range_f-range_i)/RAND_MAX);;
   for( int i=0; i<250000; i++ ) { 
-    // x_metro  = sample_pdf_metropolis( &pdf_maxwell_boltzmann, (double*)&params, range_i, range_f, x_metro);
-    x_metro  = sample_pdf_metropolis( &cdf_maxwell_boltzmann, (double*)&params, range_i, range_f, x_metro);
 
+    if (test_cdf){
+      x_metro  = sample_pdf_metropolis( &cdf_maxwell_boltzmann, (double*)&params, range_i, range_f, x_metro);
+    } else {
+      x_metro  = sample_pdf_metropolis( &pdf_maxwell_boltzmann, (double*)&params, range_i, range_f, x_metro);
+    }
     printf( "i: %d val: %lf\n", i, x_metro);
   }
 
--- cpp/probability/test/test_maxwell_boltzmann_rejection.cc
+++ cpp/probability/test/test_maxwell_boltzmann_rejection.cc
@@ -4,24 +4,41 @@
 
 unsigned count_reject=0, count_metro=0;
 
+//----------------------------------------------
+// usage: ./test_maxwell_boltzmann_rejction is [random seed] [a] [test_cdf]
+// example: ./test_maxwell_boltzmann_rejction $RANDOM 1.0 1 > maxwell_boltzmann.dat
+// ---------------------------------------------
+
 int main(int argc, char** argv) {
 
-  if( argc != 2 ) {
+  if( argc < 2 ) {
     fprintf(stderr,"Error : need a single argument (random seed)\n");
     return -1;
   }
 
   srand(strtoul(argv[1],NULL,10));
   
+  double a = 1;
+  if ( argc > 2 ) {
+    a = strtod(argv[2],NULL);
+  }
+
+  bool test_cdf = false;
+  if ( argc > 3 ) {
+    test_cdf = true;
+  }
+
   double range_i = 0.;
   double range_f =  20.;
-  double a      = 1;
   double params[1] = {a};
 
   double x;
   for( int i=0; i<250000; i++ ) { 
-    x = sample_pdf_rejection( &pdf_maxwell_boltzmann, (double*)&params, range_i, range_f);
-    // x = sample_pdf_rejection( &cdf_maxwell_boltzmann, (double*)&params, range_i, range_f);
+    if ( test_cdf ) {
+      x = sample_pdf_rejection( &cdf_maxwell_boltzmann, (double*)&params, range_i, range_f);
+    } else {
+      x = sample_pdf_rejection( &pdf_maxwell_boltzmann, (double*)&params, range_i, range_f);
+    }
     printf( "i: %d val: %lf\n", i, x);
   }
 
